055d75b5a0426ab388bad21463ab5025d9ac7a4b
Robert Kaye <rob@musicbrainz.org>
2014-01-04 06:31:14 -0800

    removing old code

--- avr/animate.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ANIMATE_H__
-#define __ANIMATE_H__
-
-void startup(void);
-void rainbow(void);
-void fade_in(void);
-
-#endif
--- avr/ledstick.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LEDSTICK_H__
-#define __LEDSTICK_H__
-
-#define NUM_LED              4
-
-void ledstick_setup(void);
-void set_led_colors(unsigned char leds[NUM_LED * 3]);
-
-#endif
