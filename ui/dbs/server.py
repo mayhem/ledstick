147b0a0a3a131addcdd2d2cf1a9fc41e2f47e05a
Robert Kaye <rob@musicbrainz.org>
2014-05-02 20:29:31 +0200

    Scale the images down to 800px wide for faster loading

--- ui/dbs/server.py
+++ ui/dbs/server.py
@@ -5,6 +5,8 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import BadRequest
 from stat import S_ISREG, ST_CTIME, ST_MODE
+import subprocess
+import uuid
 
 STATIC_PATH = "/static"
 STATIC_FOLDER = "../static"
@@ -12,6 +14,7 @@ TEMPLATE_FOLDER = "../templates"
 UPLOAD_FOLDER = '../static/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 PAGE_SIZE = 5
+MAX_IMAGE_WIDTH=800
 
 app = Flask(__name__,
             static_url_path = STATIC_PATH,
@@ -41,6 +44,15 @@ def get_image_urls(page):
     urls = [ "/static/uploads/" + f[0] for f in file_list[page * PAGE_SIZE : page * PAGE_SIZE + PAGE_SIZE]]
     return urls, page < (len(file_list) // PAGE_SIZE) 
 
+def scale_image(filename, width):
+    tf = os.path.join(UPLOAD_FOLDER, str(uuid.uuid4()))
+    if not subprocess.check_call(["convert", "-size", "%d" % width, filename, "-resize", 
+                          "%d" % width, tf]):
+        os.unlink(filename);
+        os.rename(tf, filename)
+    else:
+        print "resize failed"
+
 @app.route("/images")
 def images():
     urls, have_more = get_image_urls(0)
@@ -68,8 +80,9 @@ def chill():
 def ws_upload():
     file = request.files['file']
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(UPLOAD_FOLDER, filename))
+        filename = os.path.join(UPLOAD_FOLDER, secure_filename(file.filename))
+        file.save(filename)
+        scale_image(filename, MAX_IMAGE_WIDTH)
         return ""
     raise BadRequest("Unsupported file type")
 
