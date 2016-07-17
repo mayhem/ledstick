9257ca0fca35fde4e9bb6925dd7d5f56c94d42c0
Robert Kaye <rob@musicbrainz.org>
2016-07-17 23:35:04 +0200

    Add startup sequence, clear function

--- pi/ledstick.py
+++ pi/ledstick.py
@@ -11,6 +11,35 @@ from dotstar import Adafruit_DotStar
 
 numpixels = 144 # Number of LEDs in strip
 
+def clear(strip):
+    for row in xrange(numpixels):
+        strip.setPixelColor(row, 0)
+
+    strip.show();
+
+def startup(strip):
+
+    for repeat in xrange(5):
+        for row in xrange(numpixels / 4):
+            strip.setPixelColor(row * 4, 0xFFAA00)
+            strip.setPixelColor(row * 4 + 1, 0xFFAA00)
+            strip.setPixelColor(row * 4 + 2, 0xFF00FF)
+            strip.setPixelColor(row * 4 + 3, 0xFF00FF)
+
+        strip.show();
+        sleep(.1)
+
+        for row in xrange(numpixels / 4):
+            strip.setPixelColor(row * 4, 0xFF00FF)
+            strip.setPixelColor(row * 4 + 1, 0xFF00FF)
+            strip.setPixelColor(row * 4 + 2, 0xFFAA00)
+            strip.setPixelColor(row * 4 + 3, 0xFFAA00)
+
+        strip.show();
+        sleep(.1)
+
+    clear(strip)
+
 def load_file(filename):
 
     r=png.Reader(file=open(filename))
@@ -35,22 +64,30 @@ if len(sys.argv) < 2:
     print "Usage: %s: <png file>" % sysargv[0]
     sys.exit(-1)
 
-image = load_file(sys.argv[1])
 
 strip   = Adafruit_DotStar(numpixels, order='bgr')
 
 strip.begin()           # Initialize pins for output
-strip.setBrightness(64) # Limit brightness to ~1/4 duty cycle
+strip.setBrightness(128) # Limit brightness to ~1/4 duty cycle
+
+startup(strip)
+
+image = load_file(sys.argv[1])
 
 while True:
     for col in xrange(image['width']):
         for row in xrange(image['height']):
-            color = ord(image['pixels'][(row * 3 * image['width']) + (col * 3)]) << 16 |     \
-                    ord(image['pixels'][(row * 3 * image['width']) + (col * 3) + 1]) << 8 | \
-                    ord(image['pixels'][(row * 3 * image['width']) + (col * 3) + 2])
+            red = ord(image['pixels'][(row * 3 * image['width']) + (col * 3)])
+            green = ord(image['pixels'][(row * 3 * image['width']) + (col * 3) + 1])
+            blue = ord(image['pixels'][(row * 3 * image['width']) + (col * 3) + 2])
+
+            blue = int(blue * .9)
+
+            color = (red << 16 | green << 8 | blue)
             strip.setPixelColor(numpixels - row, color)
 
 	strip.show()
 	sleep(.0002)
 
+    clear(strip)
     sleep(.05)
